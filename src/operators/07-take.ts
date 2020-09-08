808c9e54bfe218896454c3b991f811a8460fc448
Iván González <ivangonzalezmahagamage@gmail.com>
2020-09-08 22:55:14 +0200

    Añade operator take

---
+++ src/operators/07-take.ts
@@ -0,0 +1,14 @@
+import { range } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+
+/**
+ * Take: selecciona el número de datos que le indicamos 
+ * y para el observable
+ */
+range(1, 80).pipe(
+    tap(console.log),
+    take(4)
+).subscribe({
+    next: val => console.log(`next: ${val}`),
+    complete: () => console.log('Completado')
+})
